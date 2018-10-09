178d169082e6763460a310f1b63fa8a708dc9357
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-10-09 22:55:39 +0200

    update

--- sideNav/main.js
+++ sideNav/main.js
@@ -7,14 +7,12 @@ const navBtn = document.querySelector('.nav-btn'),
 
 
       function openSlide() {
-        // document.querySelector('main').style.marginLeft = '250px';
         document.querySelector('.menu').style.width = '100%';
-        console.log('hello');
+        document.querySelector('.menu').style.visibility = 'visible';
       };
 
       function closeSlide() {
-        // document.querySelector('main').style.marginLeft = '0';
         document.querySelector('.menu').style.width = '0';
-        console.log('hello');
+        document.querySelector('.menu').style.visibility = 'hidden';
 
       };
