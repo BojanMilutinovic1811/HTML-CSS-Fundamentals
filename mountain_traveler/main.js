04266b272fe125755cad90b05059909e55469023
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-11-07 21:52:07 +0100

    playing around

--- mountain_traveler/main.js
+++ mountain_traveler/main.js
@@ -4,14 +4,11 @@ const btn = document.querySelector(".submit-btn");
 const nav = document.querySelector('nav');
 let tglClass = false;
 
-// console.log(btn, nav);
-
 
 btn.addEventListener('click', toggleMenu);
 
 
 function toggleMenu() {
-    // console.log('clicked')
     if(!tglClass) {
         nav.classList.add('show')
         tglClass = true;
