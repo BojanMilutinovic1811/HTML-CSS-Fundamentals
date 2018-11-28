1ee3766033c8c10ecfb7b63329aa5cdc6f7b87a7
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-11-28 15:27:55 +0100

    quazzu signup & solutions pages

--- sites/quazzu/js/init.js
+++ sites/quazzu/js/init.js
@@ -3,5 +3,15 @@
 
     $('.sidenav').sidenav();
 
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+    $('.modal').modal();
+    $('.tabs').tabs();
+    $('select').formSelect();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+        
