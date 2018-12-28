74f4eb40dcc76b684f1b9da4bda0cd656933844a
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-12-28 12:42:41 +0100

    update

--- sites/sunset_chaser/main.js
+++ sites/sunset_chaser/main.js
@@ -1,5 +1,4 @@
-
-
+(function() {
 const btn = document.querySelector(".submit-btn");
 const nav = document.querySelector('nav');
 let tglClass = false;
@@ -17,3 +16,31 @@ function toggleMenu() {
         tglClass = false;
     }
 }
+
+
+//button animation
+
+const particleOpt =  {
+    style: 'stroke',
+    color: 'black',
+    direction: 'bottom',
+    duration: 1200,
+    easing: 'easeOutSine',
+    speed: .7,
+    oscillationCoefficient: 5
+};
+const particles = new Particles('.button', particleOpt);
+document.querySelector('.btn').addEventListener('click', particlesActivate)
+
+function particlesActivate(e) {
+    e.preventDefault()
+    if ( !particles.isAnimating()) {
+
+        particles.disintegrate();
+    }
+    console.log('particles')
+}
+  
+// Disintegrate the button into particles
+})()
+
