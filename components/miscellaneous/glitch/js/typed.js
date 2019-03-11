ce5e0f8347e0390d2a5685fb075f21b3d78e42c0
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2019-03-11 20:13:09 +0100

    exercise

--- components/miscellaneous/glitch/js/typed.js
+++ components/miscellaneous/glitch/js/typed.js
@@ -1,7 +1,44 @@
 
-var options = {
-  stringsElement: '#typed-strings',
-  typeSpeed: 50
+
+const nameOptions = {
+  stringsElement: '#typed-name',
+  typeSpeed: 40,
+  startDelay: 1500,
+  showCursor: false
 }
 
-var typed = new Typed("#typed", options);
+const ageOptions = {
+  stringsElement: '#typed-age',
+  typeSpeed: 40,
+  startDelay: 3500,
+  showCursor: false
+}
+
+const strengthsOption = {
+  stringsElement: '#typed-strengths',
+  typeSpeed: 40,
+  startDelay: 5000,
+  showCursor: false
+
+}
+
+const weaknessesOption = {
+  stringsElement: '#typed-weaknesses',
+  typeSpeed: 40,
+  startDelay: 9000,
+  showCursor: false
+}
+
+const weaponOption = {
+  stringsElement: '#typed-weapon',
+  typeSpeed: 40,
+  startDelay: 11500,
+  showCursor: false
+}
+
+const name = new Typed('#name', 
+nameOptions)
+const age = new Typed('#age', ageOptions);
+const strengths = new Typed('#strengths', strengthsOption)
+const weaknesses = new Typed('#weaknesses', weaknessesOption)
+const weapon = new Typed('#weapon', weaponOption)
