a1691691b47eea2766e1c612f61d247fbdf25473
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2019-05-19 11:39:31 +0200

    exercise

--- sites/portfolio/js/main.js
+++ sites/portfolio/js/main.js
@@ -9,6 +9,7 @@ const menuBtn = document.getElementById("menu-btn"),
   menuItems = document.querySelectorAll(".menu-item"),
   animLetter = document.querySelector(".toAnimate");
 
+
 const workButtons = Array.from(document.querySelectorAll("button"));
 workButtons.forEach(button => button.addEventListener("click", displayWork));
 
@@ -110,6 +111,23 @@ menuItems.forEach(item => {
 
 document.addEventListener("DOMContentLoaded", dropOpen);
 
+
+
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function () {
+  var st = window.pageYOffset || document.documentElement.scrollTop;
+  if (st > lastScrollTop) {
+    console.log('down');
+    menuBtn.style.visibility = "hidden"
+  } else {
+    console.log('up');
+
+    menuBtn.style.visibility = "visible"
+  }
+  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+
 //functions
 
 function transformBtn() {
@@ -155,4 +173,4 @@ function counterClass() {
 
 // particles js
 
-particlesJS.load("particles-js", "particles.json", function() {});
+particlesJS.load("particles-js", "particles.json", function () { });
